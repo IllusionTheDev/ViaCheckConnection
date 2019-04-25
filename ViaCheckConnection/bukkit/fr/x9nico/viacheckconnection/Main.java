12520f1c374747dc556f9528181054e651e9c310
Nicolas RAYNAUD <xnico2809@gmail.com>
2019-04-25 12:33:14 +0200

    Updated 1.14 support to release

--- ViaCheckConnection/bukkit/fr/x9nico/viacheckconnection/Main.java
+++ ViaCheckConnection/bukkit/fr/x9nico/viacheckconnection/Main.java
@@ -8,7 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import us.myles.ViaVersion.api.Via;
 import us.myles.ViaVersion.api.protocol.ProtocolVersion;
 
